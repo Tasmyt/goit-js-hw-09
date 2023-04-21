eec4f758b4876dd8fa04ec0c996e691c229dca37
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-04-21 12:48:15 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const changeColor = {
+    intervalId: null,
+    isActive: false,
+    start() {
+        if (this.isActive) { return; }
+        this.isActive = true;
+        this.intervalId = setInterval(() => { refs.body.style.background = getRandomHexColor() }, 1000)
+    },
+    stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+},
+};
+refs.btnStart.addEventListener('click', () => { changeColor.start(); });
+
+refs.btnStop.addEventListener('click', () => { changeColor.stop(); });
